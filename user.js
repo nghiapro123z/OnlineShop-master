4454a5e6df8b16ab6013da98f8db935ebe14643f
nghiapro123z <nghiatmgch17633@fpt.edu.vn>
2020-06-22 10:12:14 +0700

    commit to 'master'

--- user.js
+++ user.js
@@ -5,4 +5,21 @@ router.get('/',(req,res)=>{
     res.render('login');
 })
 
+router.post("/", async (req, res) => {
+    let un = req.body.un;
+    let pass = req.body.pass;
+    if (un == "admin" && pass == "admin") {
+        let client = await MongoClient.connect(url);
+        let dbo = client.db("asm");
+
+        let result = await dbo.collection("products").find({}).toArray();
+        //res.render("allProduct", { product: result });
+        var fullUrl = req.protocol + "://" + req.get("host") + "/product";
+        res.redirect(fullUrl);
+    } else {
+        res.end("Login failed");
+    }
+});
+
+
 module.exports = router;
